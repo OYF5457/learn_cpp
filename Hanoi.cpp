20d1964cd9f0534f85bd0037db90e8bb46de0e93
OYF5457 <chenzhixiao_1992@163.com>
2016-05-25 17:36:49 +0800

    commit Hanoi.cpp

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,16 +1,17 @@
 //
 // Created by 陈志宵 on 16/5/25.
+// 汉诺塔问题
 //
 #include <iostream>
 using namespace std;
 
-void hanoi(int n, char a, char b, char c) { // 把n个盘子从a柱子借助b柱子移动到c柱子
+void hanoi(int n, char a, char b, char c) { // 把a上的n个盘子从a柱子借助b柱子移动到c柱子
     if(n == 1) {
         cout << a << "->" << c << endl;
     } else {
-        hanoi(n - 1, a, c, b);
-        cout << a << "->" << c << endl;
-        hanoi(n - 1, b, a, c);
+        hanoi(n - 1, a, c, b);// 把a上的n - 1个盘子从a柱子借助c柱子移动到b柱子
+        cout << a << "->" << c << endl; //把a上的最下面盘子移到c上
+        hanoi(n - 1, b, a, c);// 把柱子b上的n-1个盘子通过柱子a移动到柱子c
     }
 }
 
