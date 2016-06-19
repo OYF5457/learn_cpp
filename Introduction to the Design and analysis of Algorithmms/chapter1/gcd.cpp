5fce15ed3f00f988ca152909297790a620767525
OYF5457 <chenzhixiao_1992@163.com>
2016-06-19 11:09:32 +0800

    commit leetcode

--- Introduction to the Design and analysis of Algorithmms/chapter1/gcd.cpp
+++ Introduction to the Design and analysis of Algorithmms/chapter1/gcd.cpp
@@ -5,8 +5,10 @@
 
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+//欧几里得算法非递归
 int gcd1(int m, int n) {
     while(n != 0) {
         int tmp = m % n;
@@ -16,7 +18,74 @@ int gcd1(int m, int n) {
     return m;
 }
 
+//欧几里得递归
+int gcd2(int m, int n) {
+    if(n == 0) {
+        return m;
+    }
+    return gcd2(n, m % n);
+}
+
+//"埃拉托色尼筛法"
+vector<int> sieve(int n) {
+    int *arr = new int[n + 1];
+    for(int i = 2; i <= n; i++) {
+        arr[i] = i;
+    }
+    for(int i = 2; i * i <= n; ++i) {
+        if(arr[i] != 0) {
+            for(int j = i * i; j <= n; j += i) {
+                arr[j] = 0;
+            }
+        }
+    }
+    vector<int> vec;
+    for(int i = 2; i <= n; i++) {
+        if(arr[i] != 0) {
+            vec.push_back(arr[i]);
+        }
+    }
+    delete []arr;
+    return vec;
+}
+
+//分解质因数,朴素方法
+void prime_factorization(int n) {
+    int start = 2;
+    while(n != 1) {
+        if(n % start == 0) {
+            printf("%d ", start);
+            n /= start;
+        } else {
+            ++start;
+        }
+    }
+}
+
+//分解质因数
+void prime_factorization1(int n) {
+    vector<int> v = sieve(n);
+    for(int i = 0; i < v.size();) {
+        if(n % v[i] == 0) {
+            printf("%d ", v[i]);
+            n /= v[i];
+        } else {
+            ++i;
+        }
+    }
+}
+
 int main() {
     cout << gcd1(60, 24) << endl;
+    cout << gcd2(60, 24) << endl;
+    vector<int> v = sieve(25);
+    for(auto m : v) {
+        cout << m << " ";
+    }
+    cout << endl;
+    prime_factorization(24);
+    cout << endl;
+    prime_factorization1(160000);
+    cout << endl;
     return 0;
 }
